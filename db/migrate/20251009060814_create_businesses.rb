b467cba38e6804245f2ac4dee7a4484ce2c08952
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2025-10-09 06:18:24 +0000

    create business model

---
+++ db/migrate/20251009060814_create_businesses.rb
@@ -0,0 +1,13 @@
+class CreateBusinesses < ActiveRecord::Migration[8.0]
+  def change
+    create_table :businesses do |t|
+      t.string :name
+      t.text :description
+      t.string :address
+      t.string :category
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
