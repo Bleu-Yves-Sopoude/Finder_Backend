0fcad0f32ae6e214e8afa82435fc1987c59022ca
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2025-10-12 23:44:19 +0000

    add review controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,39 @@
+class ReviewsController < ApplicationController
+    before_action :authorize_request
+    before_action :set_business
+
+    # GET /businesses/:business_id/reviews
+    def index
+      reviews = @business.reviews.includes(:user).order(created_at: :desc)
+      render json: reviews.as_json(
+        include: { user: { only: [ :id, :name, :email ] } },
+        only: [ :id, :rating, :comment, :created_at ]
+      )
+    end
+
+    # POST /businesses/:business_id/reviews
+    def create
+      review = @business.reviews.build(review_params)
+      review.user = current_user
+
+      if review.save
+        render json: review.as_json(
+          include: { user: { only: [ :id, :name, :email ] } },
+          only: [ :id, :rating, :comment, :created_at ]
+        ), status: :created
+      else
+        render json: { errors: review.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+
+    private
+
+    def set_business
+      @business = Business.find(params[:business_id])
+    end
+
+    def review_params
+      params.require(:review).permit(:rating, :comment)
+    end
+end
