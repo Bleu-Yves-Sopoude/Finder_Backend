57897aacc841a7268270b9df9391737e219cfc85
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2025-10-18 23:54:50 +0000

    business list and page endpoints

---
+++ app/controllers/businesses_controller.rb
@@ -0,0 +1,43 @@
+class BusinessesController < ApplicationController
+
+    def index
+      businesses = Business.includes(:reviews).all
+
+      render json: businesses.map { |b|
+        {
+          id: b.id,
+          name: b.name,
+          category: b.category,
+          address: b.address,
+          average_rating: b.reviews.average(:rating)&.round(1)
+        }
+      }
+    end
+
+    def show
+      business = Business.includes(reviews: :user).find(params[:id])
+      average_rating = business.reviews.average(:rating)&.round(1)
+
+      render json: {
+        id: business.id,
+        name: business.name,
+        description: business.description,
+        address: business.address,
+        category: business.category,
+        average_rating: average_rating,
+        reviews: business.reviews.map do |review|
+          {
+            id: review.id,
+            rating: review.rating,
+            comment: review.comment,
+            created_at: review.created_at,
+            user: {
+              id: review.user.id,
+              name: review.user.name
+            }
+          }
+        end
+      }
+    end
+
+  end
